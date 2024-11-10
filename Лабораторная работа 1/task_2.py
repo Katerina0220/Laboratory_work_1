753f30293f7139fd3065084461a8ac686b9d12cd
Katerina0220 <ekaterinaharskaa@gmail.com>
2024-11-10 20:20:18 +0300

    Update task_2.py

--- Лабораторная работа 1/task_2.py
+++ Лабораторная работа 1/task_2.py
@@ -1,6 +1,14 @@
-book = 4 * 25 * 50 * 100
-disk = 1.44 * 1024 * 1024
-count = disk / book
+disk_size = 1.44
+Mb_in_Kb = 1024
+Kb_in_b = 1024
+symbol_weight = 4
+page = 100
+line = 50
+symbol = 25
+
+book = symbol_weight * symbol * line * page
+disk = disk_size * Mb_in_Kb * Kb_in_b
+count = (disk // book)
 
 # TODO Найдите количество книг, которое можно разместить на дискете
 
